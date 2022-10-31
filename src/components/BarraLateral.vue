f8eb7592e0a7442ec1cd77be12bbbca3f4fe9bc1
Magno Belloni <bellonidesouza6@gmail.com>
2022-10-30 21:54:39 -0300

    Versão inicial

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,60 @@
+<template>
+  <header>
+    <h1>
+      <img src="../assets/logo.png" alt="Logo Alura Tracker" />
+    </h1>
+    <button class="button" @click="alterarTema">
+      <i :class="iconeBotao" style="size:1.5rem"></i>
+      {{textoBotao}}
+    </button>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BarraLateral",
+  emits: ['aoTemaAlterado'],
+  data(){
+    return {
+      modoEscuroAtivo: false
+    }
+  },
+  computed:{
+    textoBotao(){
+      if(this.modoEscuroAtivo)
+        return 'Ativar modo escuro'
+      return 'Desativar modo escuro'
+    },
+    iconeBotao(){
+      if(this.modoEscuroAtivo)
+        return 'fa-solid fa-moon'
+      return 'fa-solid fa-sun'
+    }
+  },
+  methods: {
+    alterarTema(){
+      this.modoEscuroAtivo = !this.modoEscuroAtivo
+      this.$emit('aoTemaAlterado', this.modoEscuroAtivo)
+    }
+  }
+});
+</script>
+
+<style scoped>
+header {
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+}
+
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
---
+++ src/components/Temporizador.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="is-flex is-align-items-center is-justify-content-space-between">
+        <Cronometro :tempoEmSegundos="tempoEmSegundos" />
+        <Botao icone="fas fa-play" texto="play" :desabilitado="cronometroRodando" @clicado="iniciar"/>
+        <Botao icone="fas fa-stop" texto="stop" :desabilitado="!cronometroRodando" @clicado="finalizar"/>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Cronometro from "./Cronometro.vue"
+import Botao from "./Botao.vue"
+
+export default defineComponent({
+    name: "Temporizador",
+    components: {
+        Cronometro,
+        Botao
+    },
+    emits: ['aoTemporizadorFinalizado'],
+    data() {
+        return {
+            tempoEmSegundos: 0,
+            cronometro: 0,
+            cronometroRodando: false
+        }
+    },
+    methods: {
+        iniciar() {
+            this.cronometroRodando = true
+            this.cronometro = setInterval(() => {
+                this.tempoEmSegundos += 1
+            }, 1000)
+        },
+        finalizar() {
+            clearInterval(this.cronometro)
+            this.$emit('aoTemporizadorFinalizado', this.tempoEmSegundos)
+            this.tempoEmSegundos = 0
+            this.cronometroRodando = false
+        }
+    }
+});
+</script>
